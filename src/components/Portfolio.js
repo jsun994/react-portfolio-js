b733ced8f03634e8a866743b535345564efb2e8d
jsun <jaysun054@gmail.com>
2021-12-19 09:53:27 -0800

    added port imgs

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import proj1 from '../assets/ptblog.png';
+import proj2 from '../assets/project01.png';
+import proj3 from '../assets/project02.png';
+import proj4 from '../assets/project03.png';
+import proj5 from '../assets/project04.png';
+import proj6 from '../assets/project05.png';
 
 function Portfolio() {
     return (
         <section className="portSec">
-            <h1 id="portfolio">portfolio</h1>
+            <h1>projects</h1>
+                <div className="projects">
+
+                    <div className="row-one">
+                        <a href="https://tech-blog-pro.herokuapp.com/" className='one'>
+                            <img src={proj1} style={{ width: "80%" }} alt="cover"></img>
+                        </a>
+                        <a href="https://jsun994.github.io/password/" className='two'>
+                            <img src={proj2} style={{ width: "80%" }} alt="cover"></img>
+                        </a>
+                    </div>
+                    
+                    <div className="row-two">
+                        <a href="https://jsun994.github.io/portfolio/" className="three"> 
+                            <img src={proj3} style={{ width: "80%" }} alt="cover"></img>
+                        </a>
+    
+                        <a href="https://jsun994.github.io/work-day-scheduler/" className="four">
+                            <img src={proj4} style={{ width: "80%" }} alt="cover"></img>
+                        </a>
+                    </div>
+
+                    <div className="row-three">
+                        <a href="https://jsun994.github.io/daily-weather-forecast/" className="five">
+                            <img src={proj5} style={{ width: "80%" }} alt="cover"></img>
+                        </a>
+    
+                        <a href="https://k-owell.github.io/Project-HotTopic/" className="six">
+                            <img src={proj6} style={{ width: "80%" }} alt="cover"></img>
+                        </a>
+                    </div>
+                </div>
         </section>
     );
 }
