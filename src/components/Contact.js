9d0e12e9b5aca93cbf969c8388783d0c6dfa1576
jsun <jaysun054@gmail.com>
2021-12-18 18:32:54 -0800

    css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,8 +35,8 @@ function ContactForm() {
   };
 
   return (
-    <section>
-      <h1 data-testid="h1tag">Contact me</h1>
+    <section className="contact">
+      <h1>Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
